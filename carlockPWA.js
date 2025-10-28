38d41965152c88f32ecbf7316543f516e3f75a37
04ColyZQ8 <130797516+04ColyZQ8@users.noreply.github.com>
2025-10-28 19:34:23 -0400

    Add files via upload

--- carlockPWA.js
+++ carlockPWA.js
@@ -1,6 +1,7 @@
 async function sendCmd(action) {
-    let endpoint = "/api/" + action;
+    let endpoint = "https://carlock-backend-od8a.onrender.com/api/" + action;
     let res = await fetch(endpoint, { method: "POST" });
     let data = await res.json();
     console.log(data);
+    alert("Command sent: " + action);
 }
